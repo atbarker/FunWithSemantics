95f230dfc3b1027327d20eb554b4d4a6e57b71aa
Austen Barker <atbarker@ucsc.edu>
2018-05-21 10:36:45 -0700

    renamed stuff

--- FinalProject/C/rc4.h
+++
@@ -1,8 +0,0 @@
-#ifndef RC4_H_
-#define RC4_H_
-
-#include <stdlib.h>
-
-void rc4(unsigned char key[], size_t keySize, unsigned char *inputBuffer, unsigned char *outputBuffer, int bufferLength);
-
-#endif
--- FinalProject/C/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-FILE=${1}
-./rc4 -i $FILE -o $FILE.encrypted.out -g key.rc4
-./rc4 -i $FILE.encrypted.out -o $FILE.decrypted.out -k key.rc4
-
-diff $FILE $FILE.decrypted.out
