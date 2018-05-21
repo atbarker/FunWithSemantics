1a1acc443f4657d9610d17af237ab4d0951543cd
Austen Barker <atbarker@ucsc.edu>
2018-05-21 10:33:51 -0700

    finished with python version

--- FinalProject/Python/rc4.py
+++ FinalProject/Python/rc4.py
@@ -1,28 +1,53 @@
+import sys
 
-#key scheduling algorithm
-def ksa(State, key, keylength):
-    for i from 0 to 255:
-        State[i] = i
+#key scheduling algorithm, initializes the state
+def ksa(key):
+    keylength = len(key)
+    state = range(256)
+    for i in range(0,255):
+        state[i] = i
     j = 0
-    for i from 0 to 255:
-        j = (j + S[i] + key[i % keylength]) % 256
-        #swap s[i] and s[j]
-
+    for i in range(0,255):
+        j = (j + state[i] + key[i % keylength]) % 256
+        #swappy boi
+        state[i], state[j] = state[j], state[i] 
+    return state
 
 #psueudo-random number generator
-def prga():
+def prga(state):
     i = 0
     j = 0
-    while():
+    while True:
         i = (i+1) % 256
-        j = (j + S[i] % 256
-        #swap s[i] s[j]
-        k = S[(S[i] + S[j]) % 256]
-        return k
+        j = (j + state[i]) % 256
+        state[i], state[j] = state[j], state[i]
+        k = state[(state[i] + state[j]) % 256]
+        yield k
+
+def key_to_ord(s):
+    return [ord(c) for c in s]
 
 
 def main():
 
+    key = 'key'
+    plain = 'yippee ki yay motherf*ckers'
+    ciphertext = [len(plain)]
+
+    key = key_to_ord(key)
+    
+    S = ksa(key)
+    keystream = prga(S)
+
+    print("Plaintext:")
+    print(plain)
+    
+    print("Ciphertext:")
+    for c in plain:
+        sys.stdout.write("%02X" % (ord(c) ^ keystream.next()))
+    
+    print
+    
 
 if __name__ == "__main__":
     main()
