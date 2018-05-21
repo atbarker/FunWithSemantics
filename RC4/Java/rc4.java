f3e884d92e7b5eabb03b07e1751572b3bcea7265
Austen Barker <atbarker@ucsc.edu>
2018-05-21 13:31:22 -0700

    finished java implementation

--- RC4/Java/rc4.java
+++ RC4/Java/rc4.java
@@ -1,16 +1,16 @@
 
 public class rc4{
 
-    public byte[] state = new byte[256];
-    public byte[] t = new byte[256];
+    public static byte[] state = new byte[256];
+    public static byte[] t = new byte[256];
 
     //key scheduling algorithm 
-    public void ksa(byte[] key){
+    public static void ksa(byte[] key){
         int keylen = key.length;
 	int j;
 	byte t;
 	for (int i = 0; i < 256; i++){
-            state[i] = (byte) i;
+            state[i] =  (byte)i;
         }
 	j = 0;
 	for (int i = 0; i < 256; i++){
@@ -22,27 +22,41 @@ public class rc4{
     }
 
     //pseudo-random number generator
-    public byte[] encrypt(byte[] key, byte[] input){
+    public static byte[] encrypt(byte[] key, byte[] input){
         int i = 0;
 	int j = 0;
 	byte t, k;
-	byte[] output = new byte[input.len];
-	for(int m = 0; m < input.len; m++){
-            i = (i+1) % 256;
-	    j = (j+state[i]) % 256;
+	byte[] output = new byte[input.length];
+	for(int m = 0; m < input.length; m++){
+            i = (int)((i+1) % 256);
+	    j = (int)(((j+state[i]) % 256) & 0xFF);
             t = state[i];
 	    state[i] = state[j];
 	    state[j] = t;
-	    k = state[(state[i] + state[j]) % 256];
-	    output[m] = input[m] ^ k;
+	    k = state[(int)(((state[i] + state[j]) % 256) & 0xFF)];
+	    output[m] = (byte)(input[m] ^ k);
 	}
 	return output;
     }
 
     //main method
     public static void main(String[] args){
+        byte[] output;
+        String key = "thisisakey";
+        String plaintext = "yippee ki yay motherf*cker";
+        byte[] keybytes = key.getBytes();
+        byte[] plainbytes = plaintext.getBytes();
+        
+        System.out.println(plaintext);
 
+        ksa(keybytes);
 
+        output = encrypt(keybytes, plainbytes);
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : output) {
+            sb.append(String.format("%02X ", b));
+        }
+        System.out.println(sb.toString());
     }
-
 }
